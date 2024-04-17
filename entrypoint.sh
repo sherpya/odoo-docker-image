c25521bca19368c5994781f0aafa01f7be409ca3
Gianluigi Tiesi <sherpya@gmail.com>
2024-04-17 20:31:35 +0200

    various fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,17 @@ set -e
 umask 0077
 
 if [ -z "${ODOO_DB_USER}" -o -z "${ODOO_DB_PASSWORD}" -o -z "${ODOO_DB_HOST}" ]; then
-    echo "Missing db user, password or host"
+    echo "Missing db user, password or host!"
     exit 1;
+fi
+
+if [ ! -z ${ODOO_DATA_DIR} ]; then
+    echo "Warning overriding odoo data_dir is not recommended!"
+fi
+
+if [ -z ${ODOO_ADMIN_PASSWD} ]; then
+    ODOO_ADMIN_PASSWD=$(tr -dc 'A-Za-z0-9!?%=' < /dev/urandom | head -c 10)
+    echo "Admin password is unset! A temporary random one will used: ${ODOO_ADMIN_PASSWD}"
 fi
 
 ODOO_ADDONS_PATH="/odoo/app/addons,/odoo/app/odoo/addons,/mnt/extra-addons"
@@ -16,16 +25,13 @@ for dir in /odoo/addons/*; do
 done
 
 ODOO_LIST_DB=${ODOO_LIST_DB:=True}
-ODOO_ADMIN_PASSWD=${ODOO_ADMIN_PASSWD:=$(tr -dc 'A-Za-z0-9!?%=' < /dev/urandom | head -c 10)}
 ODOO_WORKERS=${ODOO_WORKERS:=0}
 ODOO_MAX_CRON_THREADS=${ODOO_MAX_CRON_THREADS:=1}
+ODOO_DATA_DIR=${ODOO_DATA_DIR:=/odoo/data}
+ODOO_PROXY_MODE=${ODOO_PROXY_MODE:=True}
 
 (
-cat << EOF
-[options]
-data_dir = /odoo/data
-proxy_mode = True
-EOF
+echo '[options]'
 
 for var in ${!ODOO_@}; do
     key=$(echo $var | sed 's/^ODOO_//' | tr '[:upper:]' '[:lower:]')
